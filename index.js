1d691775ede03b60a44fa509dfba0efae2495ab6
Masaaki Morishita <ikasama3.0@gmail.com>
2014-12-13 09:38:18 +0900

    first implement

--- index.js
+++ index.js
@@ -1,2 +1,47 @@
-module.exports = function () {
+var parse = require('css-annotation').parse
+
+var inspect = require('obj-inspector')
+
+module.exports = function plugin (css, options) {
+    options = options || {}
+
+    var annotations = parse(css)
+    var important = annotations[0].important
+
+    return function (root) {
+        root.eachRule(function (rule) {
+            if (checkImportant(rule)) {
+                if (important === true) {
+                    rule.each(function (child) {
+                        if (child.type === 'decl') {
+                            child.important = true
+                        }
+                    })
+                }
+                else {
+                    rule.each(function (child) {
+                        if (child.type === 'decl') {
+                            important.forEach(function (property) {
+                                if (child.prop === property) {
+                                    child.important = true
+                                }
+                            })
+                        }
+                    })
+                }
+            }
+        })
+    }
+}
+
+function checkImportant (rule) {
+    if (rule.childs) {
+        var children = rule.childs
+        var text = ''
+        children.forEach(function (child) {
+            if (child.type === 'comment') text = child.text
+        })
+        if (text.match(/\@important/)) return true
+    }
+    return false
 }
--- test/index.js
+++ test/index.js
@@ -1,6 +1,23 @@
-var test = require('colored-tape')
-var postcss-important = require('..')
+var fs = require('fs')
+var test = require('tape')
+var postcss = require('postcss')
+var important = require('..')
 
-test('description', function (t) {
-  t.end()
-})
+function fixture (name) {
+    return fs.readFileSync('test/fixtures/' + name + '.css', 'utf-8').trim()
+}
+
+function output (name) {
+    return fs.readFileSync('test/fixtures/' + name + '.out.css', 'utf-8').trim()
+}
+
+function compare (name) {
+    return test(name, function (t) {
+        var res = postcss().use(important(fixture(name))).process(fixture(name)).css.trim()
+        t.same(res, output(name))
+        t.end()
+    })
+}
+
+compare('test-1')
+compare('test-2')
